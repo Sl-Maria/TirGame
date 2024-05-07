4a98e329770f0544315c11e1cfe3b9cffc127483
Sl-Maria <168527483+Sl-Maria@users.noreply.github.com>
2024-05-07 15:05:43 +0300

    add score and smoth movement

--- Main.py
+++ Main.py
@@ -15,8 +15,17 @@ target_width = 58
 target_height = 50
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+target_speed_x = random.choice([-1, 1])  # Уменьшенная скорость
+target_speed_y = random.choice([-1, 1])  # Уменьшенная скорость
 
-color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+score = 0
+font = pygame.font.Font(None, 36)
+fail_score = 0
+
+clock = pygame.time.Clock()  # Создание объекта Clock для контроля кадров
+clock_speed = 30             # Скорость кадров
 
 running = True
 while running:
@@ -27,9 +36,30 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1
+                clock_speed += 5
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                target_speed_x = random.choice([-1, 1])
+                target_speed_y = random.choice([-1, 1])
+            else:
+                fail_score += 1
+
+    target_x += target_speed_x
+    target_y += target_speed_y
+
+    if target_x < 0 or target_x > SCREEN_WIDTH - target_width:
+        target_speed_x = -target_speed_x
+    if target_y < 0 or target_y > SCREEN_HEIGHT - target_height:
+        target_speed_y = -target_speed_y
+
     screen.blit(target_image, (target_x, target_y))
+    score_text = font.render(f"Попадания: {score}", True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+    fail_score_text = font.render(f"Промахи: {fail_score}", True, (255, 255, 255))
+    screen.blit(fail_score_text, (10, 35))
+
     pygame.display.update()
+    clock.tick(clock_speed)  # Ограничение до 60 кадров в секунду
 
 pygame.quit()
